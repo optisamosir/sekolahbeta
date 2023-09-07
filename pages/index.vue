10adf1fb916a242c466e8462106658e320dd6756
optiprabina <opti.git@gmail.com>
2023-09-07 15:50:40 +0700

    pesan commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,131 +1,90 @@
 <template>
 <section>
-  <div class ="py-4">
-    <div class="container">
-      <div class="title border-bottom d-flex align-item-center justify-content-between py-2">
-        <h1>Tugas 3</h1>
-        <div class="d-flex align-item-center">
-          <span class="me-2">View As</span>
-          <button
-          class="btn btn-outline-secondary py-1 px-3"
-          @click="isGrid=!isGrid">
-          {{ isGrid?'Grid':'List' }}
-          </button>
-        </div>
-      </div>
-    </div>
+  <div class="title border-bottom d-flex align-items-center py-2">
+    <h5>Task</h5>
+    <div class="d-flex align-items-center ms-auto">
+      Pilih Semester
+      <div class="d-flex align-items-center justify-content-end w-100">
+        <select class="form-select" v-model="selectedCategory" placeholder="Pilih Semester">
+          
+          <option value="1">Semester 1</option>
+          <option value="2">Semester 2</option>
+        </select>    </div>
   </div>
-  <div class="list-task row">
-    <div 
-    :class="[
-    'item-task d-flex align-items-start border-bottom pt-3 pb-4',
-    isGrid? 'col-12 col-md-6 col-lg-4' :'col-12']">
-<!-- // v-model berfungsi untuk mengikat variabel yang dideklarasikan dan secara
-otomatis akan memperbaharui elemen berdasarkan tipe input -->
-<input
-id="task"
-type="checkbox"
-name="status"
-class="me-2 mt-2"
-:checked="tasks[0].isDone"
-v-model="tasks[0].isDone"
->
-<div
-:class="['d-flex flex-column', tasks[0].isDone ?
-'text-decoration-line-through fst-italic' : '']">
-<div class="title-task mb-1">
-{{ tasks[0].title }}
 </div>
-<div class="description-task small text-muted">
-{{ tasks[0].description }}
-</div>
-</div>
-</div>
-
-<div 
-    :class="[
-    'item-task d-flex align-items-start border-bottom pt-3 pb-4',
-    isGrid? 'col-12 col-md-6 col-lg-4' :'col-12']">
-<!-- // v-model berfungsi untuk mengikat variabel yang dideklarasikan dan secara
-otomatis akan memperbaharui elemen berdasarkan tipe input -->
-<input
-id="task"
-type="checkbox"
-name="status"
-class="me-2 mt-2"
-:checked="tasks[1].isDone"
-v-model="tasks[1].isDone"
->
-<div
-:class="['d-flex flex-column', tasks[1].isDone ?
-'text-decoration-line-through fst-italic' : '']">
-<div class="title-task mb-1">
-{{ tasks[0].title }}
-</div>
-<div class="description-task small text-muted">
-{{ tasks[0].description }}
-</div>
-</div>
-</div>
-
-<div 
-    :class="[
-    'item-task d-flex align-items-start border-bottom pt-3 pb-4',
-    isGrid? 'col-12 col-md-6 col-lg-4' :'col-12']">
-<!-- // v-model berfungsi untuk mengikat variabel yang dideklarasikan dan secara
-otomatis akan memperbaharui elemen berdasarkan tipe input -->
-<input
-id="task"
-type="checkbox"
-name="status"
-class="me-2 mt-2"
-:checked="tasks[2].isDone"
-v-model="tasks[2].isDone"
->
-<div
-:class="['d-flex flex-column', tasks[2].isDone ?
-'text-decoration-line-through fst-italic' : '']">
-<div class="title-task mb-1">
-{{ tasks[0].title }}
-</div>
-<div class="description-task small text-muted">
-{{ tasks[0].description }}
-</div>
-</div>
-</div>
-
+<div class="list-task row">
+  <CardItem
+  v-for="(task, i) in resultQuery"
+  :key="i"
+  :task="task"
+  :isGrid="isGrid"
+  />
 </div>
 </section>
 </template>
 
 <script>
-export default{
-data(){
+import CardItem from "@/components/Card/CardItem.vue"
+export default {
+  components: {
+    CardItem
+  },
+  data(){
   return{
-    isGrid : false,
-    isCreating : false,
+    selectedCategory:'',
+    searchQuery: '',
+    isCreating: false,
+    isGrid: false,
     tasks : [
       {
         title :'Kelas X',
-        description : 'Pembelajaran Kelas X',
-        category : 'Tugas Harian',
+        description : 'Ulangan',
+        category : 'Semester 1',
+        isDone :false,
+      },
+      {
+        title :'Kelas X',
+        description : 'Ulangan',
+        category : 'Semester 2',
         isDone :false,
       },
       {
         title :'Kelas XI',
-        description : 'Pembelajaran Kelas XI',
-        category : 'Tugas Harian',
+        description : 'Ulangan',
+        category : 'Semester 1',
+        isDone :false,
+      },
+      {
+        title :'Kelas XI',
+        description : 'Ulangan',
+        category : 'Semester 2',
         isDone :false,
       },
       {
         title :'Kelas XII',
-        description : 'Pembelajaran Kelas XII',
-        category : 'Tugas Harian',
+        description : 'Ulangan',
+        category : 'Semester 1',
+        isDone :false,
+      },
+      {
+        title :'Kelas XII',
+        description : 'Ulangan',
+        category : 'Semester 2',
         isDone :false,
       }
     ]
   }
+},
+computed :  {
+    resultQuery() {
+      if (!this.selectedCategory) {
+        return this.tasks;
+      } else {
+        return this.tasks.filter((item)=> {
+          return item.category.includes(this.selectedCategory);
+        });
+      }
 }
-
+}
 }
 </script>
